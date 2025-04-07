4e7bd2053aa342ed65392253598c47b2865b12ce
devnom-git <nauman.sajjad@devfor.io>
2025-04-07 18:52:55 +0000

    lifeline of goffix

---
+++ lifeline.php
@@ -0,0 +1,10 @@
+<?php
+$projects = [
+    'goffixlifeline' => 'active'
+];
+
+$project_id = $_GET['project'] ?? '';
+$status = $projects[$project_id] ?? 'disabled';
+
+header('Content-Type: application/json');
+echo json_encode(['status' => $status]);
